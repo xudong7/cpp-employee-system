efd17abee1114b033cce615e9539b1e691e914e4
xudong7 <13433126742@163.com>
2024-10-30 17:51:26 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,170 @@
+#include "person.cpp"
+
+int Person::count = 0;
+
+void setColor(int color)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void colorTextForOneLine(string text, int color)
+{
+    setColor(color);
+    cout << text;
+    setColor(7);
+}
+
+void insertAll(EmployeeList& list)
+{
+    string name;
+    setColor(12);
+    cout << "********************************" << endl;
+    cout << "This Action Will Clear Old Data!!!!" << endl;
+    cout << "********************************" << endl;
+    setColor(7);
+    cout << "Are you sure to insert all employees? (y/n): ";
+    char choice;
+    cin >> choice;
+    if (choice != 'y') return;
+    list.clear();
+    colorTextForOneLine("***Inserting employees***\n", 10);
+    cout << "please enter the name of Manager: ";
+    cin >> name;
+    list.addEmployee(new Manager(name));
+    cout << "please enter the name of Technician, exit if enter @" << endl;
+    while (true)
+    {
+        cout << "the name of Technician: ";
+        cin >> name;
+        if (name != "@") 
+            list.addEmployee(new Technician(name));
+        else 
+            break;
+    }
+    cout << "please enter the name of SalesManager, exit if enter @: " << endl;
+    while (true)
+    {
+        cout << "the name of SalesManager: ";
+        cin >> name;
+        if (name != "@") 
+            list.addEmployee(new SalesManager(name));
+        else 
+            break;
+    }
+    cout << "please enter the name of SaleMan, exit if enter @: " << endl;
+    while (true)
+    {
+        cout << "the name of SaleMan: ";
+        cin >> name;
+        if (name != "@") 
+            list.addEmployee(new SaleMan(name));
+        else 
+            break;
+    }
+    colorTextForOneLine("Inserting employees done.\n", 10);
+}
+
+void addEmployee(EmployeeList& list)
+{
+    string name;
+    int grade;
+    colorTextForOneLine("***Adding new employee***\n", 10);
+    cout << "please enter employee name and grade(4 Manager 3 Technician 2 SalesManager 1 SaleMan): ";
+    cin >> name >> grade;
+    switch (grade)
+    {
+        case 4:
+            list.addEmployee(new Manager(name));
+            break;
+        case 3:
+            list.addEmployee(new Technician(name));
+            break;
+        case 2:
+            list.addEmployee(new SalesManager(name));
+            break;
+        case 1:
+            list.addEmployee(new SaleMan(name));
+            break;
+        default:
+            cout << "Invalid grade." << endl;
+    }
+    colorTextForOneLine("Adding new employee done.\n", 10);
+}
+
+void deleteEmployee(EmployeeList& list)
+{
+    int id;
+    colorTextForOneLine("***Deleting employee***\n", 10);
+    cout << "please enter the ID of employee: ";
+    cin >> id;
+    list.deleteByID(id);
+    colorTextForOneLine("Deleting employee done.\n", 10);
+}
+
+void insertSalary(EmployeeList& list)
+{
+    int workHours;
+    double sales;
+    colorTextForOneLine("***Inserting salary***\n", 10);
+    list.setWorkHour();
+    list.setSales();
+    colorTextForOneLine("Inserting salary done.\n", 10);
+}
+
+void displaySalary(EmployeeList& list)
+{
+    colorTextForOneLine("***Displaying salary table***\n", 10);
+    list.display();
+    colorTextForOneLine("Displaying salary table done.\n", 10);
+}
+
+void menu()
+{
+    EmployeeList list;
+    string loadFileName = "employees.txt";
+    list.loadFromFile(loadFileName);
+    while (true)
+    {
+        colorTextForOneLine("Company Employee Manage System\n", 3);
+        cout << "--------------------------------" << endl;
+        cout << "1. Insert All Employee" << endl;
+        cout << "2. Add new Employee" << endl;
+        cout << "3. delete Employee" << endl;
+        cout << "4. Insert Salary" << endl;
+        cout << "5. Display Salary Table" << endl;
+        cout << "0. Exit" << endl;
+        cout << "--------------------------------" << endl;
+        cout << "Please choose: ";
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+            case 1:
+                insertAll(list);
+                break;
+            case 2:
+                addEmployee(list);
+                break;
+            case 3:
+                deleteEmployee(list);
+                break;
+            case 4:
+                insertSalary(list);
+                break;
+            case 5:
+                displaySalary(list);
+                break;
+            case 0:
+                cout << "Goodbye!" << endl;
+                list.saveToFile("employees.txt");
+                return;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    menu();
+}
---
+++ person.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+class Person
+{
+protected:
+    string name;
+    int id = 2001;
+    int grade;
+    double salary;
+    static int count;
+
+public:
+    Person(string name, int grade) : name(name), grade(grade)
+    {
+        id += count;
+        count++;
+    }
+    string getName() { return name; }
+    int getID() { return id; }
+    int getGrade() { return grade; }
+    double getSalary() { return salary; }
+    static int getCount() { return count; }
+    void display()
+    {
+        printf("%-6d%-8s%-9d%-10.2f\n", id, name.c_str(), grade, salary);
+    };
+    virtual void setSalary() = 0;
+    virtual void setWorkHours(int workHours) {}
+    virtual void setSales(double sales) {}
+    virtual ~Person() {}
+};
+
+class SaleMan : virtual public Person
+{
+protected:
+    double sales;
+
+public:
+    SaleMan(string name, double sales = 0) : Person(name, 1), sales(sales)
+    {
+        setSalary();
+    }
+    double getSales() { return sales; }
+    virtual void setSalary() { this->salary = 0.04 * sales; }
+    void setSales(double sales) { this->sales = sales; }
+};
+
+class Technician : virtual public Person
+{
+protected:
+    int workHours;
+
+public:
+    Technician(string name, int workHours = 0) : Person(name, 3), workHours(workHours)
+    {
+        setSalary();
+    }
+    int getWorkHours() { return workHours; }
+    virtual void setSalary() { this->salary = 100 * workHours; }
+    void setWorkHours(int workHours) { this->workHours = workHours; }
+};
+
+class Manager : virtual public Person
+{
+public:
+    Manager(string name) : Person(name, 4)
+    {
+        setSalary();
+    }
+    virtual void setSalary() { this->salary = 18000; }
+};
+
+class SalesManager : virtual public SaleMan
+{
+public:
+    SalesManager(string name, double sales = 0) : SaleMan(name, sales), Person(name, 2)
+    {
+        setSalary();
+    }
+    virtual void setSalary() { this->salary = 0.05 * sales + 5000; }
+};
+
+class EmployeeList
+{
+protected:
+    vector<Person *> employees;
+
+public:
+    void addEmployee(Person *p)
+    {
+        employees.push_back(p);
+    }
+    void display()
+    {
+        cout << "ID    " << "Name    " << "Grade    " << "Salary" << endl;
+        cout << "--------------------------------" << endl;
+        for (Person *p : employees)
+        {
+            p->display();
+            cout << "--------------------------------" << endl;
+        }
+    }
+    void deleteByName(string name)
+    {
+        for (int i = 0; i < employees.size(); i++)
+        {
+            if (employees[i]->getName() == name)
+            {
+                employees.erase(employees.begin() + i);
+                cout << "Employee " << name << " has been deleted." << endl;
+                return;
+            }
+        }
+        cout << "Employee " << name << " not found." << endl;
+    }
+    void deleteByID(int id)
+    {
+        for (int i = 0; i < employees.size(); i++)
+        {
+            if (employees[i]->getID() == id)
+            {
+                employees.erase(employees.begin() + i);
+                cout << "Employee with ID " << id << " has been deleted." << endl;
+                return;
+            }
+        }
+        cout << "Employee with ID " << id << " not found." << endl;
+    }
+    void setWorkHour()
+    {
+        for (Person *p : employees)
+        {
+            Technician *t = dynamic_cast<Technician *>(p);
+            if (t)
+            {
+                int workHours;
+                cout << "Enter work hours for " << t->getName() << ": ";
+                cin >> workHours;
+                t->setWorkHours(workHours);
+                t->setSalary();
+            }
+        }
+    }
+    void setSales()
+    {
+        for (Person *p : employees)
+        {
+            SaleMan *s = dynamic_cast<SaleMan *>(p);
+            if (s)
+            {
+                double sales;
+                cout << "Enter sales for " << s->getName() << ": ";
+                cin >> sales;
+                s->setSales(sales);
+                s->setSalary();
+            }
+        }
+    }
+    vector<Person *> getEmployees() { return employees; }
+    void clear()
+    {
+        employees.clear();
+    }
+    void saveToFile(string filename)
+    {
+        ofstream file(filename);
+        for (Person *p : employees)
+        {
+            file << p->getID() << " " << p->getName() << " " << p->getGrade() << " " << p->getSalary() << endl;
+        }
+        file.close();
+    }
+    void loadFromFile(const string &filename)
+    {
+        ifstream file(filename);
+        if (!file.is_open())
+        {
+            cerr << "Error opening file: " << filename << endl;
+            return;
+        }
+
+        int id, grade;
+        double salary;
+        string name;
+
+        while (file >> id >> name >> grade >> salary)
+        {
+            Person *employee = nullptr;
+            switch (grade)
+            {
+            case 1:
+                employee = new SaleMan(name, salary / 0.04); 
+                break;
+            case 2:
+                employee = new SalesManager(name, salary - 5000); 
+                break;
+            case 3:
+                employee = new Technician(name, salary / 100); 
+                break;
+            case 4:
+                employee = new Manager(name);
+                break;
+            default:
+                cerr << "Invalid grade for employee: " << name << endl;
+                continue; 
+            }
+            if (employee)
+            {
+                employee->setSalary(); 
+                employees.push_back(employee);
+            }
+        }
+
+        file.close();
+        cout << "Employee data loaded from " << filename << "..." << endl;
+    }
+};
